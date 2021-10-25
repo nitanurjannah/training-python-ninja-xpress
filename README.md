b90f029730f382a94ba2c75835dbe17fc0cf3559
Nita Nurjannah <nitanurjannah@Nitas-MacBook-Air.local>
2021-10-25 16:26:44 +0700

    Simpan semua file pert-2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # training-python-ninja-xpress
 training python
---
+++ pert-2.py
@@ -0,0 +1,55 @@
+# name = input ("Enter name: ")
+# print("Your name is", name)
+
+# OPERASI TERDAPAT TIPE DATA (STRING)
+
+# my_string = "Hello World!"
+
+# print(len(my_string))
+# print(my_string[0:5])
+
+# my_string = my_string.replace("World","Friend")
+# print(my_string)
+
+# a = "Hello"
+# b = "World!"
+# c = a + " " + b
+
+# print(c)
+
+# a = "Siap"
+# b = 86
+# c = a + " " + str(b)
+
+# print(c)
+
+# nama = "Nita"
+# alamat = "BSD"
+
+# my_profile= "Nama saya {} dan saat ini saya tinggal di {}.".format(nama,alamat)
+
+# print(my_profile)
+
+# x = "Indonesia AI"
+
+# print(x[2:4])
+
+# BOOLEANS
+a = 10
+b = 9
+
+if b > a:
+    print("b is greater than a")
+
+else:
+    print("b is not greater than a")
+
+# LIST
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+
+print(my_list)
+
+
